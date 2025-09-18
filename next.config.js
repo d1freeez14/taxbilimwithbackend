3b65cd3f87c7185149bd3f2c8b5311bc9869db2a
didar <d.zhetpisbayev@documentolog.com>
2025-09-19 01:40:25 +0500

    fixes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_URL: 'http://localhost:5001'
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: ['/root', '/tmp', '/var', '**/node_modules']
+      }
+    }
+    return config
   }
 }
 
